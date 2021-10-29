ae465099ad023670cb41dd15b910a531e7de6b0e
hcsylvester <64375193+hcsylvester@users.noreply.github.com>
2021-10-29 13:41:29 -0500

    Update library.py

--- library.py
+++ library.py
@@ -7,39 +7,37 @@ def scores(playersName, i):
     # Read all lines into list above
     fixedWidthFile = open("topPlayers.txt", "r")
     for eachLine in fixedWidthFile.readlines():
-        players.append(eachLine)
+        score = eachLine[0:10].rstrip().lstrip()
+        player = eachLine[10:20].rstrip().lstrip()
+        myList = [score, player]
+        players.append(myList)
+        #players.append(eachLine)
 
     # Close so no mishaps occur
     fixedWidthFile.close()
 
     # Add new players score
-    players.append(f"{i}         {playersName}")
-
-    ###################### This is one way I tried but cannot figure out from here or if even right    ######################
-
-    # for eachLine in players:
-    #     [score, player] = eachLine.split(" ")
-    #     score = "0000000000" + player
-    #     score = score[-10:]
-    #     player = player.strip()
-    #     players.append(score + " " + player)
+    #players.append(f"{i}         {playersName}")
+    players.append([str(i), playersName])
 
     # sort correctly
-    ###################### I found this lambda function online, and it does what I want the first time but still issues  ######################
-    # You can switch between my lambda and this one and see the results, not sure which is better at this point
-    #players.sort(key = lambda x: int("".join([i for i in x if i.isdigit()]))) # If you use this one, it works the first time but then the txt file is messed up.
     players.sort(key = lambda x: (int(x[0])))
-    # I can put substitute and it will work
+
+    # Replit Code 8 for reference
     for eachLine in players[0:5]:
-        score = eachLine[0:10]
-        player = eachLine[10:20]
-        players.append([score, player])
-        print(eachLine)
+        [score, player] = eachLine
+        score = score + "          "
+        new = score + player
+        players.append(new)
+        print(f"{score}{player}")
+        #print(eachLine)
+
 
     #We need to write new scores to the txt file
     fixedWidthFile = open("topPlayers.txt", "w")
-    for eachLine in players:
-        fixedWidthFile.write(eachLine)
+    for eachLine in players[0:5]:
+        fixedWidthFile.write(str(eachLine) + "\n")
 
     fixedWidthFile.close()
     #File assignment review and writing back out
+
